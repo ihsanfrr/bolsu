e32664b25e56b51e2b27443d5e7b200396b9965b
ihsanfrr <ihsan.fajardan@gmail.com>
2023-07-05 20:40:23 +0700

    Menambahkan Semua Halaman dan Terhubung ke Database

--- src/alpro2/BOLSU.java
+++ src/alpro2/BOLSU.java
@@ -14,7 +14,7 @@ public class BOLSU {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new AuthenticationScreen().setVisible(true);
     }
     
 }
--- src/alpro2/StockManagementScreen.java
+++ src/alpro2/StockManagementScreen.java
@@ -1,20 +1,19 @@
-package alpro2;
-
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+package alpro2;
 
 /**
  *
  * @author ihsanfrr
  */
-public class LoginScreen extends javax.swing.JFrame {
+public class StockManagementScreen extends javax.swing.JFrame {
 
     /**
-     * Creates new form LoginScreen
+     * Creates new form StockManagementScreen
      */
-    public LoginScreen() {
+    public StockManagementScreen() {
         initComponents();
     }
 
@@ -60,20 +59,20 @@ public class LoginScreen extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StockManagementScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StockManagementScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StockManagementScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(LoginScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(StockManagementScreen.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new LoginScreen().setVisible(true);
+                new StockManagementScreen().setVisible(true);
             }
         });
     }
